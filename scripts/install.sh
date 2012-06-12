cb9ac371176989b1aab97fe74fb5047c2480cb17
Steven Farrel <sfarrell@cern.ch>
2012-06-12 07:57:23 +0000

    make install script executable

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,3 +9,4 @@ svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/Sus
 # This will take a while!
 svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/Multilepton/trunk/fiveinvfb/MultiLep MultiLep
 source MultiLep/installscripts/install_script.sh
+
