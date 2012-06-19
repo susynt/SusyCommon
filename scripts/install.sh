32d41ad8d961767da8cd05bd1d4905c39e765b40
Steven Farrel <sfarrell@cern.ch>
2012-06-19 23:49:04 +0000

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,6 @@ svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/Sus
 
 # All this does is checkout the common code and run the install script there
 # This will take a while!
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/Multilepton/trunk/fiveinvfb/MultiLep MultiLep
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/Multilepton/trunk/twentyinvfb/MultiLep MultiLep
 source MultiLep/installscripts/install_script.sh
 
