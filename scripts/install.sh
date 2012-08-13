7548f71e1e5c23432e02039a0f099e2a0a48de5a
Steven Farrel <sfarrell@cern.ch>
2012-08-13 12:43:16 +0000

    update README and install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Run this from your work dir, not within any package
+# Run this from your work dir, not within any package.
+
+# This script will check out the trunk of these packages.
+# If you want to check out a specific tag, modify the commands accordingly.
 
 # Checkout SusyNtuple
 svn co svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyNtuple/trunk SusyNtuple
 
 # All this does is checkout the common code and run the install script there
 # This will take a while!
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/Multilepton/trunk/twentyinvfb/MultiLep MultiLep
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/trunk MultiLep
 source MultiLep/installscripts/install_script.sh
 
