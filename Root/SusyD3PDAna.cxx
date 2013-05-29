ed94dd6aa8ad2270e4e2ca4ff164cab605f9430a
Steven Farrel <sfarrell@cern.ch>
2013-05-29 22:50:08 +0000

    update GRL

--- Root/SusyD3PDAna.cxx
+++ Root/SusyD3PDAna.cxx
@@ -130,7 +130,11 @@ void SusyD3PDAna::Begin(TTree* /*tree*/)
   // GRL
   if(!m_isMC){
     if(m_grlFileName.Length() == 0){
-      m_grlFileName = gSystem->ExpandPathName("$ROOTCOREDIR/data/MultiLep/data12_8TeV.periodAllYear_DetStatus-v58-pro14-01_DQDefects-00-00-33_PHYS_StandardGRL_All_Good.xml");
+      //m_grlFileName = gSystem->ExpandPathName
+      //("$ROOTCOREDIR/data/MultiLep/data12_8TeV.periodAllYear_DetStatus-v58-pro14-01_DQDefects-00-00-33_PHYS_StandardGRL_All_Good.xml");
+      string grlName = "$ROOTCOREDIR/data/";
+      grlName += "data12_8TeV.periodAllYear_DetStatus-v61-pro14-02_DQDefects-00-01-00_PHYS_StandardGRL_All_Good.xml";
+      m_grlFileName = gSystem->ExpandPathName(grlName.c_str());
     }
     
     Root::TGoodRunsListReader* grlReader = new Root::TGoodRunsListReader();
