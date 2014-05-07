fccffa984fa5f29325e441bd0ecf6d28506dde2b
Steve Farrell <sfarrell@cern.ch>
2014-05-07 02:54:04 +0200

    update install script for MultiLep package tag

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,6 @@
 
 # All this does is checkout the common code and run the install script there
 # This will take a while!
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-01-02 MultiLep
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-01-03 MultiLep
 source MultiLep/installscripts/install_script.sh
 
