088a67d1e922825895451d6475f27b27e1596ead
gerbaudo <davide.gerbaudo@gmail.com>
2015-09-30 16:38:01 +0200

    drop spaces around <<

--- util/NtMaker.cxx
+++ util/NtMaker.cxx
@@ -24,89 +24,89 @@ using Susy::SusyNtMaker;
 
 void help()
 {
-  cout << "  Options:"                          << endl;
-  cout << "  -n number of events to process"    << endl;
-  cout << "     defaults: -1 (all events)"      << endl;
+  cout<<"  Options:"                         <<endl;
+  cout<<"  -n number of events to process"   <<endl;
+  cout<<"     defaults: -1 (all events)"     <<endl;
 
-  cout << "  -k number of events to skip"       << endl;
-  cout << "     defaults: 0"                    << endl;
+  cout<<"  -k number of events to skip"      <<endl;
+  cout<<"     defaults: 0"                   <<endl;
 
-  cout << "  -d debug printout level"           << endl;
-  cout << "     defaults: 0 (quiet) "           << endl;
+  cout<<"  -d debug printout level"          <<endl;
+  cout<<"     defaults: 0 (quiet) "          <<endl;
 
-  cout << "  -f name of input filelist"         << endl;
-  cout << "     defaults: fileList.txt"         << endl;
+  cout<<"  -f name of input filelist"        <<endl;
+  cout<<"     defaults: fileList.txt"        <<endl;
 
-  cout << "  -s sample name, sets isMC flag"    << endl;
-  cout << "     use e.g. 'ttbar', 'DataG', etc" << endl;
+  cout<<"  -s sample name, sets isMC flag"   <<endl;
+  cout<<"     use e.g. 'ttbar', 'DataG', etc"<<endl;
 
-  cout << "  -w set sum of mc weights for norm" << endl;
-  cout << "     default: 1"                     << endl;
+  cout<<"  -w set sum of mc weights for norm"<<endl;
+  cout<<"     default: 1"                    <<endl;
 
-  cout << "  -x set cross section"              << endl;
-  cout << "     default: -1 (use susy db)"      << endl;
+  cout<<"  -x set cross section"             <<endl;
+  cout<<"     default: -1 (use susy db)"     <<endl;
 
-  cout << "  -l set lumi"                       << endl;
-  cout << "     default: 5312/pb"               << endl;
+  cout<<"  -l set lumi"                      <<endl;
+  cout<<"     default: 5312/pb"              <<endl;
 
-  cout << "  -m write output ntuple"            << endl;
-  cout << "     default: 1 (true)"              << endl;
+  cout<<"  -m write output ntuple"           <<endl;
+  cout<<"     default: 1 (true)"             <<endl;
 
-  cout << "  --grl specify GRL"                 << endl;
-  cout << "     default: set in cxx"            << endl;
+  cout<<"  --grl specify GRL"                <<endl;
+  cout<<"     default: set in cxx"           <<endl;
 
-  cout << "  --sys will turn on systematic run" << endl;
-  cout << "     default: off"                   << endl;
+  cout<<"  --sys will turn on systematic run"<<endl;
+  cout<<"     default: off"                  <<endl;
 
-  cout << "  --savePh will save photons"        << endl;
-  cout << "     default: off"                   << endl;
+  cout<<"  --savePh will save photons"       <<endl;
+  cout<<"     default: off"                  <<endl;
 
-  cout << "  --saveTau will save taus"          << endl;
-  cout << "     default: off"                   << endl;
+  cout<<"  --saveTau will save taus"         <<endl;
+  cout<<"     default: off"                  <<endl;
 
-  cout << "  --saveContTau will save container" << endl;
-  cout << "     taus instead of selected"       << endl;
+  cout<<"  --saveContTau will save container"<<endl;
+  cout<<"     taus instead of selected"      <<endl;
 
-  cout << "  --saveTruth will save truth"       << endl;
-  cout << "     default: off"                   << endl;
+  cout<<"  --saveTruth will save truth"      <<endl;
+  cout<<"     default: off"                  <<endl;
 
-  cout << "  --af2 specifies AF2 samples"       << endl;
-  cout << "     default: off"                   << endl;
+  cout<<"  --af2 specifies AF2 samples"      <<endl;
+  cout<<"     default: off"                  <<endl;
 
-  cout << "  --d3pd1032 sets d3pd tag to p1032" << endl;
-  cout << "     default: p1181"                 << endl;
+  cout<<"  --d3pd1032 sets d3pd tag to p1032"<<endl;
+  cout<<"     default: p1181"                <<endl;
 
-  cout << "  --metFlav set met flavor"          << endl;
-  cout << "     default: Default"               << endl;
+  cout<<"  --metFlav set met flavor"         <<endl;
+  cout<<"     default: Default"              <<endl;
 
-  //cout << "  --useMetMuons set to use met muons"<< endl;
-  //cout << "     for calculating missing energy "<< endl;
-  //cout << "     default: off"                   << endl;
+  //cout<<"  --useMetMuons set to use met muons"<< endl;
+  //cout<<"     for calculating missing energy "<< endl;
+  //cout<<"     default: off"                  <<endl;
 
-  //cout << "  --doMetFix turn on MET ele-jet"    << endl;
-  //cout << "     overlap fix"                    << endl;
-  //cout << "     default: off"                   << endl;
+  //cout<<"  --doMetFix turn on MET ele-jet"   <<endl;
+  //cout<<"     overlap fix"                   <<endl;
+  //cout<<"     default: off"                  <<endl;
 
-  cout << "  --filterOff turns off filtering"   << endl;
-  cout << "     default: filter is on"          << endl;
+  cout<<"  --filterOff turns off filtering"  <<endl;
+  cout<<"     default: filter is on"         <<endl;
 
-  cout << "  --nLepFilter number of light leps" << endl;
-  cout << "     to filter on. Default: 0"       << endl;
+  cout<<"  --nLepFilter number of light leps"<<endl;
+  cout<<"     to filter on. Default: 0"      <<endl;
 
-  cout << "  --nLepTauFilter number of total"   << endl;
-  cout << "     light+tau to filter on."        << endl;
-  cout << "     Default: 2"                     << endl;
+  cout<<"  --nLepTauFilter number of total"  <<endl;
+  cout<<"     light+tau to filter on."       <<endl;
+  cout<<"     Default: 2"                    <<endl;
 
-  cout << "  --filterTrig turns on trigger"     << endl;
-  cout << "     filtering."                     << endl;
+  cout<<"  --filterTrig turns on trigger"    <<endl;
+  cout<<"     filtering."                    <<endl;
 
-  cout << "  --trig set which triggers are  "   << endl;
-  cout << "    stored.                      "   << endl;
-  cout << "    Default: 1 (0: Run1, 1:Run2) "   << endl;
-  cout << "--input  name of the input container" <<endl;
-  cout << "--output name of the output container"<<endl;
-  cout << "--tag    SustNtuple production tag"   <<endl;
-  cout << "  -h print this help"                << endl;
+  cout<<"  --trig set which triggers are  "  <<endl;
+  cout<<"    stored.                      "  <<endl;
+  cout<<"    Default: 1 (0: Run1, 1:Run2) "  <<endl;
+  cout<<"--input  name of the input container" <<endl;
+  cout<<"--output name of the output container"<<endl;
+  cout<<"--tag    SustNtuple production tag"   <<endl;
+  cout<<"  -h print this help"               <<endl;
 }
 
 
@@ -135,8 +135,8 @@ int main(int argc, char** argv)
   string trigset     = "run2";
   string inputContainer, outputContainer, ntTag;
 
-  cout << "SusyNtMaker" << endl;
-  cout << endl;
+  cout<<"SusyNtMaker"<<endl;
+  cout<<endl;
 
   int optind(1);
   while(optind < argc) {
@@ -173,31 +173,31 @@ int main(int argc, char** argv)
       optind++;
   } // while(optind)
 
-  cout << "flags:" << endl;
-  cout << "  sample        " << sample   << endl;
-  cout << "  nEvt          " << nEvt     << endl;
-  cout << "  nSkip         " << nSkip    << endl;
-  cout << "  dbg           " << dbg      << endl;
-  cout << "  fileList      " << fileList << endl;
-  cout << "  grl           " << grl      << endl;
-  cout << "  sys           " << sysOn    << endl;
-  cout << "  savePh        " << savePh   << endl;
-  cout << "  saveTau       " << saveTau  << endl;
-  cout << "  saveContTau   " << saveContTau << endl;
-  cout << "  saveTru       " << saveTruth<< endl;
-  cout << "  isAF2         " << isAF2    << endl;
-  cout << "  d3pdtag       " << tag      << endl;
-  cout << "  metFlav       " << metFlav  << endl;
-  cout << "  lumi          " << lumi     << endl;
-  cout << "  filter        " << filter   << endl;
-  cout << "  nLepFilter    " << nLepFilter    << endl;
-  cout << "  nLepTauFilter " << nLepTauFilter << endl;
-  cout << "  filterTrig    " << filterTrig    << endl;
-  cout << "  triggerSet    " << trigset       << endl;
-  cout << "  input         " << inputContainer  << endl;
-  cout << "  output        " << outputContainer << endl;
-  cout << "  ntTag         " << ntTag           << endl;
-  cout << endl;
+  cout<<"flags:"<<endl;
+  cout<<"  sample        "<<sample  <<endl;
+  cout<<"  nEvt          "<<nEvt    <<endl;
+  cout<<"  nSkip         "<<nSkip   <<endl;
+  cout<<"  dbg           "<<dbg     <<endl;
+  cout<<"  fileList      "<<fileList<<endl;
+  cout<<"  grl           "<<grl     <<endl;
+  cout<<"  sys           "<<sysOn   <<endl;
+  cout<<"  savePh        "<<savePh  <<endl;
+  cout<<"  saveTau       "<<saveTau <<endl;
+  cout<<"  saveContTau   "<<saveContTau<<endl;
+  cout<<"  saveTru       "<<saveTruth<< endl;
+  cout<<"  isAF2         "<<isAF2   <<endl;
+  cout<<"  d3pdtag       "<<tag     <<endl;
+  cout<<"  metFlav       "<<metFlav <<endl;
+  cout<<"  lumi          "<<lumi    <<endl;
+  cout<<"  filter        "<<filter  <<endl;
+  cout<<"  nLepFilter    "<<nLepFilter   <<endl;
+  cout<<"  nLepTauFilter "<<nLepTauFilter<<endl;
+  cout<<"  filterTrig    "<<filterTrig   <<endl;
+  cout<<"  triggerSet    "<<trigset      <<endl;
+  cout<<"  input         "<<inputContainer <<endl;
+  cout<<"  output        "<<outputContainer<<endl;
+  cout<<"  ntTag         "<<ntTag          <<endl;
+  cout<<endl;
 
 
   // Build the input chain
@@ -236,13 +236,13 @@ int main(int argc, char** argv)
 
   // Run the job
   if(nEvt<0) nEvt = nEntries;
-  cout << endl;
-  cout << "Total entries:   " << nEntries << endl;
-  cout << "Process entries: " << nEvt << endl;
+  cout<<endl;
+  cout<<"Total entries:   "<<nEntries<<endl;
+  cout<<"Process entries: "<<nEvt<<endl;
   chain->Process(susyAna, sample.c_str(), nEvt, nSkip);
 
-  cout << endl;
-  cout << "SusyNtMaker job done" << endl;
+  cout<<endl;
+  cout<<"SusyNtMaker job done"<<endl;
 
   delete chain;
   return 0;
